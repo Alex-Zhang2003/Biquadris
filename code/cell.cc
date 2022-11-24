6af123102e14600c103b2e425d276b3258411933
Jason Yu <Jason.Yu1@uwaterloo.ca>
2022-11-24 14:10:41 -0500

    test Jason

--- code/cell.cc
+++ code/cell.cc
@@ -1,6 +1,39 @@
 #include "cell.h"
 
 
+Cell::Cell(int x, int y, char c, bool placed):
+    x{x}, y{y}, c{c}, placed{placed} {}
+
+bool Cell::isEmpty() const {
+    return placed;
+}
+
+char Cell::getChar() const {
+    if (!isEmpty()) return c;
+}
+
+void Cell::setChar(char val) {
+    c = val;
+    placed = true;
+}
+
+int Cell::getRow() const {
+    return x;
+}
+
+void Cell::setRow(int row) {
+    x = row;
+}
+
+int Cell::getCol() const {
+    return y;
+}
+
+void Cell::setCol(int col) {
+    y = col;
+}
+
+Cell::~Cell() { }
 
 
 
--- code/cell.h
+++ code/cell.h
@@ -6,12 +6,14 @@ class Cell {
     char c;
     bool placed;
 public:
+    Cell(int x, int y, char c, bool placed);
     bool isEmpty() const;
     char getChar() const;
-    void setChar(char val) const;
+    void setChar(char val);
     int getRow() const;
     void setRow(int row);
     int getCol() const;
     void setCol(int col);
+    ~Cell();
 };
 
